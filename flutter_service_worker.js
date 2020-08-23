6138037638989dac03edfc94d7f909d7d9f0f698
stanleyillidge <stanleyillidge@users.noreply.github.com>
2020-08-23 12:40:48 +0000

    deploy: 010de904cfe95b0a57fb01edb892800b2ee9d7b5

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "3d64b2afc92476cf0834a49493565622",
+"main.dart.js": "63e826da2c91f07e6b8c3a1c854fa48a",
 "index.html": "e6337217a53c8073692ceb40d96dbf9e",
 "/": "e6337217a53c8073692ceb40d96dbf9e",
 "manifest.json": "d9e6cf4c425faf6b99d42915e6abac6f",
