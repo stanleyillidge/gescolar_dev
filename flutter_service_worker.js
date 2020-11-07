1f2d0efbcc73fc740bd339a301f118ef4d55a4d6
stanleyillidge <stanleyillidge@users.noreply.github.com>
2020-11-07 15:57:46 +0000

    deploy: cc5f5e85c54a6922ecb42fc7e4d8777da94cf2e4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -58,10 +58,10 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/AssetManifest.json": "5c0d7baa177259562f7882b5b32f5964",
 "manifest.json": "d9e6cf4c425faf6b99d42915e6abac6f",
-"main.dart.js": "fdc70664f427d9c43a78906d63e01ec9",
+"main.dart.js": "99034aecc30f3a85b07c8240e1ec56d1",
 "version.json": "8acff96db90bee780d831b64c8c43fca",
-"index.html": "c06f7f3de2af81b8ef0ff39db5caa4ec",
-"/": "c06f7f3de2af81b8ef0ff39db5caa4ec",
+"index.html": "98112bed955d7ed515787019d8c04b1c",
+"/": "98112bed955d7ed515787019d8c04b1c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
 };
